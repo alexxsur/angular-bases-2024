f76b0890a1c22d63fcc094f10b54f0b7d7c1ab96
Alex <alexxsur@gmail.com>
2024-03-20 12:08:42 -0400

    Counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'Mi App Angular 2024';
   public counter: number = 10;
+
+  updateCounter(value : number = 1):void {
+    this.counter += value;
+  }
+
+  resetCounter():void {
+    this.counter = 0;
+  }
+
+
 }
