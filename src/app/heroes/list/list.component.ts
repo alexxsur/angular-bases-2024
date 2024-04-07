d36808783127b3520fbbe98cdec4f8ccb6828e43
Alex <alexxsur@gmail.com>
2024-04-07 18:27:21 -0400

    ngfor directive

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  public heroNames: string[] =
+  [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'She Hulk',
+    'Thor'
+  ];
+
+
 }
