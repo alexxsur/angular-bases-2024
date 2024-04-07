4adddb056a3f5a723eab8c90ce73205eeda6f29a
Alex <alexxsur@gmail.com>
2024-04-07 16:38:32 -0400

    Counter component functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'Mi App Angular 2024';
-  public counter: number = 10;
-
-  updateCounter(value : number = 1):void {
-    this.counter += value;
-  }
-
-  resetCounter():void {
-    this.counter = 0;
-  }
-
-
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -2,8 +2,21 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola counter</h1>'
+  template: `<h3> {{ counter }} </h3>
+  <button (click)="updateCounter(+1)">+1</button>
+  <button (click)="resetCounter()">Reset</button>
+  <button (click)="updateCounter(-1)">-1</button>`
 })
 export class CounterComponent {
+
+  public counter: number = 10;
+
+  updateCounter(value : number = 1):void {
+    this.counter += value;
+  }
+
+  resetCounter():void {
+    this.counter = 0;
+  }
 
 }
