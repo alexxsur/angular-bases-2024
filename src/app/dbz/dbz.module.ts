3ab567107b08867f9dfde7e1b47c6359f97b66a1
Alex <alexxsur@gmail.com>
2024-04-14 21:44:48 -0400

    dbz list component

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
 
 
 
 @NgModule({
   declarations: [
+        MainPageComponent,
+        ListComponent
+    ],
+    exports: [
     MainPageComponent
   ],
   imports: [
