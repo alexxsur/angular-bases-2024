89a74fa90486361ce91a6787f00513f202aaea10
Alex <alexxsur@gmail.com>
2024-04-28 15:47:38 -0400

    FormsModule and ngModel

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,3 +1,4 @@
+import { Character } from './../../interfaces/character.interface';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './add-character.component.html',
   styleUrl: './add-character.component.css',
 })
-export class AddCharacterComponent { }
+export class AddCharacterComponent {
+
+  public character: Character = {
+    name: '',
+    power: 0
+  }
+
+  emitCharacter() : void {
+    console.log(this.character)
+  }
+}
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,21 +3,23 @@ import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
 import { AddCharacterComponent } from "./components/add-character/add-character.component";
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-    declarations: [
-        MainPageComponent,
-        ListComponent,
-        AddCharacterComponent
-    ],
-    exports: [
-        MainPageComponent
-    ],
-    imports: [
-        CommonModule
-    ]
+  declarations: [
+    MainPageComponent,
+    ListComponent,
+    AddCharacterComponent
+  ],
+  exports: [
+    MainPageComponent
+  ],
+  imports: [
+    FormsModule,
+    CommonModule
+  ]
 })
 export class DbzModule {
 
